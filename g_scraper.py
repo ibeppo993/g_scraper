5b764c216a31ca581648752aa53b56b7ad39a012
Giuseppe Ferrari <giuseppe.ferrari@evoluzionetelematica.it>
2022-02-20 12:27:47 +0100

    Update g_scraper.py

--- g_scraper.py
+++ g_scraper.py
@@ -12,9 +12,14 @@ proxy_pass = 'Ta802Ta802'
 
 # Create a new instance of the Chrome driver
 option = webdriver.ChromeOptions()
+#option.add_argument("--headless")
 option.add_argument('--incognito')
 option.add_argument("--window-size=1920,1080")
+option.add_argument('--disable-blink-features=AutomationControlled')
+option.add_experimental_option("excludeSwitches", ["enable-automation"])
+option.add_experimental_option('useAutomationExtension', False)
 
+keyword = get_keyword()
 proxy = get_proxy()
 print(proxy)
 options_seleniumWire = {
@@ -27,6 +32,8 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),optio
 # Go to the Google home page
 url = 'https://www.ilmioip.it/'
 driver.get(url)
+driver.explicit_wait(5)
+
 
 # Access requests via the `requests` attribute
 for request in driver.requests:
@@ -39,3 +46,9 @@ for request in driver.requests:
            request.response.status_code,
            request.response.headers['Content-Type']
         )
+
+HTML_DOM = driver.execute_script("return document.documentElement.outerHTML")
+
+with open(f'html_output/{keyword}.html', 'w+') as f:
+    f.write(HTML_DOM)
+    f.close()
